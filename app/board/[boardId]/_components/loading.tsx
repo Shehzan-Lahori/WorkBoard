b5e8a77d7bbb2b41b56c83f8ad8c4fb92bc22ba3
Shehzan-Lahori <lahorishehzan3@gmail.com>
2024-06-23 14:48:53 +0530

    final changes

---
+++ app/board/[boardId]/_components/loading.tsx
@@ -0,0 +1,15 @@
+import { InfoSkeleton } from "./info";
+import { Loader } from "lucide-react";
+import { ParticipantsSkeleton } from "./participants";
+import { ToolbarSkeleton } from "./toolbar";
+
+export const Loading = () => {
+  return (
+    <main className=" h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
+      <Loader className="h-6 w-6 text-muted-foregorund animate-spin" />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
+    </main>
+  );
+};
